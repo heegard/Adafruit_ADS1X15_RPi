7ced6994c876928f93eb22d20ffc2e0bdc5ad21a
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-08-30 09:18:43 +0000

    Allow negative integers in differential example
    This is how it should be since differential will also
    measure negative voltages.

--- examples/differential_rpi.c
+++ examples/differential_rpi.c
@@ -9,25 +9,14 @@ Adafruit_ADS1115 ads;
 
 int main(int argc, char *argv[])
 {
-  uint16_t adc0;
+  int16_t adc0;
 
-  ads.setGain(GAIN_TWOTHIRDS);
-  usleep(500000);
+  ads.setGain(GAIN_EIGHT);
   ads.begin();
-  usleep(500000);
   while (true) {
-    adc0 = ads.readADC_SingleEnded(0);
-        //unsigned char adc0c = adc0;
-        //printf("adc0c before: ");
-        //printBits(sizeof(adc0c), &adc0c);
-        //adc0c = ((adc0c << 8) & 0xFF00) | ((adc0c >> 8) & 0x00FF);
-        //printf("\nadc0c  after: ");
-    //printBits(sizeof(adc0), &adc0);
-    //printf(" *** %d\n", adc0);
-    uint16_t adc0r = adc0;
-    adc0r = (adc0r >> 8) | (adc0r << 8);
-    printBits(sizeof(adc0r), &adc0r);
-    printf(" ||| %d\n", adc0r);
+    adc0 = ads.readADC_Differential_0_1();
+    printBits(sizeof(adc0), &adc0);
+    printf(" *** %d\n", adc0);
     usleep(100000);
   }
 }
--- examples/singleended_rpi.c
+++ examples/singleended_rpi.c
@@ -11,23 +11,12 @@ int main(int argc, char *argv[])
 {
   uint16_t adc0;
 
-  ads.setGain(GAIN_TWO);
-  usleep(500000);
+  ads.setGain(GAIN_EIGHT);
   ads.begin();
-  usleep(500000);
   while (true) {
     adc0 = ads.readADC_SingleEnded(0);
-        //unsigned char adc0c = adc0;
-        //printf("adc0c before: ");
-        //printBits(sizeof(adc0c), &adc0c);
-        //adc0c = ((adc0c << 8) & 0xFF00) | ((adc0c >> 8) & 0x00FF);
-        //printf("\nadc0c  after: ");
-    //printBits(sizeof(adc0), &adc0);
-    //printf(" *** %d\n", adc0);
-    uint16_t adc0r = adc0;
-    //adc0r = (adc0r >> 8) | (adc0r << 8);
-    printBits(sizeof(adc0r), &adc0r);
-    printf(" ||| %d\n", adc0r);
+    printBits(sizeof(adc0), &adc0);
+    printf(" *** %d\n", adc0);
     usleep(100000);
   }
 }
